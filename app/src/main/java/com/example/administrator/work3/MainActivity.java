c7a22cd9ba9a90b0aba20f09242dcd1a095ba056
Zheng Weizhan <907652300@qq.com>
2015-10-22 22:38:51 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/work3/MainActivity.java
@@ -0,0 +1,112 @@
+package com.example.administrator.work3;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    private Button button;
+    private EditText editText;
+    private CheckBox checkBox;
+    private CheckBox checkBox2;
+    private TextView textView5;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button= (Button) findViewById(R.id.button);
+        editText= (EditText) findViewById(R.id.editText);
+        checkBox= (CheckBox) findViewById(R.id.checkBox);
+        checkBox2= (CheckBox) findViewById(R.id.checkBox2);
+        textView5= (TextView) findViewById(R.id.textView5);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        registerEvent();
+    }
+    private void  registerEvent(){
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!editText.getText().toString().trim().equals("")) {
+                    if (checkBox.isChecked() || checkBox2.isChecked()) {
+                        Double weight = Double.parseDouble(editText.getText().toString());
+                        StringBuffer sb = new StringBuffer();
+                        sb.append("----评估结果----\n");
+                        if (checkBox.isChecked()) {
+                            sb.append("男性标准身高：");
+                            Double result = evaluateHeight(weight, "男");
+                            sb.append(result + "(厘米)");
+                        }
+                        if (checkBox2.isChecked()) {
+                            sb.append("女性标准身高：");
+                            Double result = evaluateHeight(weight, "女");
+                            sb.append(result + "(厘米)");
+                        }
+                        textView5.setText(sb.toString());
+                    } else {
+                        showMessage("请选择性别！");
+                    }
+                } else {
+                    showMessage("请输入体重！");
+                }
+            }
+
+
+        });
+    }
+
+    private Double evaluateHeight(Double weight, String sex) {
+        Double height;
+        if(sex=="男"){
+            height=170-(62-weight)/0.6;
+        }else{
+            height=158-(52-weight)/0.5;
+        }
+        return height;
+    }
+    private void showMessage(String message) {
+        AlertDialog alert = new AlertDialog.Builder(this).create();
+        alert.setTitle("系统信息");
+        alert.setMessage(message);
+        alert.setButton("确定", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+        alert.show();
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+       menu.add(Menu.NONE,1,Menu.NONE,"退出");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()){
+            case 1:
+                finish();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
